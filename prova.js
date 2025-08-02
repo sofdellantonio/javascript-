92783b2cf6cfd7e95870563b11efeefdb3d279d8
sofdellantonio <sofiadellantonio77@gmail.com>
2025-08-02 16:58:23 +0000

    calcoli

--- prova.js
+++ prova.js
@@ -5,14 +5,27 @@
     prezzo: 12,
     allergeni:["lattosio", "glutine"],
     note: ""
-});**/
+});*/
+
  let button = document.querySelector('button');
  let title = document.querySelector('h1');
+ let numero = document.querySelector('input');
 
-function bottone(){
-title.innerHTML = "Ciao Sofia";
+/** function bottone(nome = "Marco"){
+title.innerHTML = `Ciao ${nome}`;
 
 }
-button.addEventListener("click", ()=>{bottone});
+button.addEventListener("click", ()=>{bottone(nome.value)});
+ */
+function cubo(number){
+let result = number*number*number;
+console.log(result);
+return result;
+}
+button.addeEventListener('click', ()=>{
+   
+    title.innerHTML = cubo(numero.value);
+});
+
 //al click del bottone deve venire fuori ciao nome a random
 
