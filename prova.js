150e80d052d5f0cb2f40368451486db3953f1fa3
sofdellantonio <sofiadellantonio77@gmail.com>
2025-08-02 17:01:58 +0000

    cubo calcolato

--- prova.js
+++ prova.js
@@ -1,31 +1,18 @@
 
-// definire un oggetto 
-/**let piatto = ({
-    nome:"Gnocchi di Polenta con ragu di coniglio",
-    prezzo: 12,
-    allergeni:["lattosio", "glutine"],
-    note: ""
-});*/
-
  let button = document.querySelector('button');
  let title = document.querySelector('h1');
  let numero = document.querySelector('input');
 
-/** function bottone(nome = "Marco"){
-title.innerHTML = `Ciao ${nome}`;
-
-}
-button.addEventListener("click", ()=>{bottone(nome.value)});
- */
 function cubo(number){
 let result = number*number*number;
 console.log(result);
 return result;
 }
-button.addeEventListener('click', ()=>{
-   
-    title.innerHTML = cubo(numero.value);
-});
 
-//al click del bottone deve venire fuori ciao nome a random
+button.addEventListener('click', ()=>{
+    title.innerHTML = cubo(numero.value);
+})
+
+
+
 
