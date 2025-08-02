50c3b8c2c628bd0e22c11144aefa8ae5dd39cddd
sofdellantonio <sofiadellantonio77@gmail.com>
2025-08-02 11:10:26 +0000

    piatti

--- prova.js
+++ prova.js
@@ -1,15 +1,8 @@
 
 // definire un oggetto 
-let persona = {
-    nome: "Federica", 
-    cognome: "Borghi",
-    codiceFiscale: "DFKJJ07LF493",
-    eta: 21,
-    cittaNascita: "Forli",
-    coloriPreferiti: ["blu", "rosa", "verde", "arancione"]
-};
-console.log(persona.nome);
-let testo = "Ciao sono una variabile";
-let elemento = `<h1>${testo}</h1`;
-
-document.body.innerHTML = elemento;
+let piatto = ({
+    nome:"Gnocchi di Polenta con ragu di coniglio",
+    prezzo: 12,
+    allergeni:["lattosio", "glutine"];
+    note: ""
+});
