831eedd8125cfae363439d3da54f814437ef241f
sofdellantonio <sofiadellantonio77@gmail.com>
2025-08-02 10:03:26 +0000

    changes

--- prova.js
+++ prova.js
@@ -1,2 +1,13 @@
 let nome = "ciao";
 let cognome;
+
+// definire un oggetto 
+let persona = {
+    nome: "Federica", 
+    cognome: "Borghi",
+    codiceFiscale: "DFKJJ07LF493",
+    eta: 21,
+    cittaNascita: "Forli",
+    coloriPreferiti: ["blu", "rosa", "verde", "arancione"]
+};
+console.log(persona.nome);
