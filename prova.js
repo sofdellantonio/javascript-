dbda65e574f9c2cc57d6224e662122692d29c98a
sofdellantonio <sofiadellantonio77@gmail.com>
2025-08-02 14:56:12 +0000

    v

--- prova.js
+++ prova.js
@@ -1,8 +1,18 @@
 
 // definire un oggetto 
-let piatto = ({
+/**let piatto = ({
     nome:"Gnocchi di Polenta con ragu di coniglio",
     prezzo: 12,
     allergeni:["lattosio", "glutine"],
     note: ""
-});
+});**/
+ let button = document.querySelector('button');
+ let title = document.querySelector('h1');
+
+function bottone(){
+title.innerHTML = "Ciao Sofia";
+
+}
+button.addEventListener("click", ()=>{bottone});
+//al click del bottone deve venire fuori ciao nome a random
+
