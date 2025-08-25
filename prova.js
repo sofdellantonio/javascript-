fcbc01aced7cf34d9af69528ac148541a4bd14d0
sofdellantonio <sofiadellantonio77@gmail.com>
2025-08-25 10:23:57 +0000

    css funzionante

--- prova.js
+++ prova.js
@@ -9,7 +9,9 @@ console.log(result);
 return result;
 }
  
+//risponde quando il bottone viene cliccato chiamando la funzione cubo
 button.addEventListener('click', ()=>{
+    //title.innerHtml fa diventare il titolo già inserito il risultato della funzione cubo
     title.innerHTML = cubo(numero.value);
 })
 
