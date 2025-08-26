4f8c106ebdde2d5bb77f4738dee705731e2ca8b4
sofdellantonio <sofiadellantonio77@gmail.com>
2025-08-26 10:38:33 +0000

    log e config

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-let express = require(express); //importo il modulo express nel codice 
+
+const express = require('express'); //importo il modulo express nel codice 
+const socket = require('socket.io');
+
+const app = express(); //app è un istanza di express
